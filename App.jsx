84b1796c26875245709a8594e5605d147cf8c9d1
Drakonmark7 <166941941+Drakonmark7@users.noreply.github.com>
2024-11-12 20:56:36 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import MyComponent from './components/MainCompo1'
+
+class App extends React.Component{
+render(){
+  return (
+    <>
+      
+      <MyComponent></MyComponent>
+    </>
+  )
+}
+}
+
+export default App
